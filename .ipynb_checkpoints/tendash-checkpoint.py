e886eb35bc7dfdf9a3aadc8ee5b68e33eea85aa8
Ben Cherian <cherian.b59@gmail.com>
2022-09-14 20:12:27 +1000

    make sim loop more efficient

--- .ipynb_checkpoints/tendash-checkpoint.py
+++ .ipynb_checkpoints/tendash-checkpoint.py
@@ -17,7 +17,7 @@ server.secret_key ='test'
 
 app = dash.Dash(name = __name__, server = server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server 
-app.title = "Child Support Formula"
+app.title = "Tennis Simulator"
 
 header = html.Div(
         children=[
@@ -64,7 +64,7 @@ match_inputs = dbc.Col(
                   html.Div(children="Number of sets", className="menu-title"),
                   dcc.Slider(id="num_set_i", min=3,max=5,value=3,step=2,tooltip={"placement": "bottom", "always_visible": False},),
 #TODO add sets
-#TODO make player b serve first
+#TODO add option for player b to serve first
               ]
           ),
         ],
@@ -97,7 +97,6 @@ set_hist = html.Div(
             className="card",
         )
               
-
 ##TODO there should be an easier way to do this.
 @app.callback(
     [Output('match_probability-container', 'children'),Output("set-distribution", "figure")],
@@ -113,22 +112,23 @@ set_hist = html.Div(
 def simulate_matches(num_set , a_base_p , a_point_streak ,  b_base_p ,  b_point_streak ):
            
     # number of iterations
-    sims = 10000
+    sims = 1000
     
     #iterate through matches
-    results = []
+    results = [0] * sims
     #numpy vectoristion is not much faster
+    default_player_profile["win_serve_p"] = a_base_p
+    default_player_profile["point_streak_adv"] = a_point_streak
+
+    rf_player_profile["win_serve_p"] = b_base_p
+    rf_player_profile["point_streak_adv"] = b_point_streak
+
     for i,income in enumerate(range(0,sims)):
 
-      default_player_profile["win_serve_p"] = a_base_p
-      default_player_profile["point_streak_adv"] = a_point_streak
-
-      rf_player_profile["win_serve_p"] = b_base_p
-      rf_player_profile["point_streak_adv"] = b_point_streak
       match = tensim.Match(1, i , player_profiles = [default_player_profile,rf_player_profile], set_limit = num_set)
       match.run_match()
-      results.append(match.a.match)
-      del match              
+      results[i] = (match.a.match)
+      #del match              
     
     #
     wins = sum(results)
@@ -137,7 +137,7 @@ def simulate_matches(num_set , a_base_p , a_point_streak ,  b_base_p ,  b_point_
     # Create figure with secondary y-axis
     fig = make_subplots()
     
-    fig.add_trace(
+    '''fig.add_trace(
         go.Scatter(x=[wins], y=[sims], # replace with your own data source
         name="Entitlement"), secondary_y=False,
     )
@@ -170,7 +170,7 @@ def simulate_matches(num_set , a_base_p , a_point_streak ,  b_base_p ,  b_point_
     )
   )
 
-
+'''
 
     return([matches_statement,fig])
    
